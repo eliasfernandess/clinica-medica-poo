0c5d2452eb2967753d1d88121b36a27b072af05a
Elias <eliascoelhogf@gmail.com>
2024-12-02 14:48:19 -0300

    CRIEI ENTIDADES CONVENIO

---
+++ src/main/java/br/edu/imepac/administrativo/entidades/Convenio.java
@@ -0,0 +1,23 @@
+package br.edu.imepac.administrativo.entidades;
+
+public class Convenio {
+
+    private int id;
+    private String descricao;
+
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+}
